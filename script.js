acb6278a97f00e1d6f308a22f1978bd815f5cb2a
Glitch (learn-code-files) <none>
2020-10-21 22:25:47 +0000

    Updated!!!

--- script.js
+++ script.js
@@ -12,30 +12,30 @@ window.onload = function() {
 document.getElementById("next-button").onclick = function() {
   if (news == 1) {
     next.innerHTML =
-      "<p>First of all, the front end is faster and a lot more stylish.</p>";
+      "<p>First of all, the front end is faster and a lot more stylish. It is so beautiful now. You can also see our SVG art that I make.</p>";
   }
   if (news == 2) {
     next.innerHTML =
-      "<p>Next, I changed the code so that the site is completely, 100%, static!</p>";
+      "<p>Next, I changed the code so that the site is completely, 100%, static! It now runs with 99%.9% uptime, with little bugs!</p>";
   }
   if (news == 3) {
     next.innerHTML =
-      "<p>You can now download an HTML file and submit it. It might even get featured on the site!</p>";
+      "<p>You can now download an HTML file and submit it. It might even get featured on the site, and then in the Gallery! Send us a message by submitting the form!</p>";
   }
   if (news == 4) {
     next.innerHTML =
-      "<p>We now have a support email: support@learn-code.me!</p>";
+      "<p>Learn Code now has a support email: support@learn-code.me! You can also feel free to chat with us. The forum also like chats, too.</p>";
+  }
+  if (news == 4) {
+    next.innerHTML =
+      "<p>Soon, I will release download instructions for complete, 100%, full offline access. Remember, we are completely free forever, and we run on donations 😉</p>";
   }
   if (news == 5) {
     next.innerHTML =
-      "<p>Any accepted projects will be put in a gallery. We are currently rolling out more of our UI, so get ready!</p>";
+      "<p>Any accepted projects will be put in a gallery. We are currently rolling out more of our UI, so get ready! You can also feel free to take the source.</p>";
+  document.getElementById('next-button').innerHTML = "Complete!";
   }
   if (news == 6) {
-    next.innerHTML =
-      "<p>Finally, give me your <a href='https://support.glitch.com/t/the-blockly-html-code-editor-learn-code-domain-released/25935?u=alt' target='_blank'>feedback</a>!</p>";
-    document.getElementById('next-button').innerHTML = "Complete!"
-  }
-  if (news == 7) {
     document.getElementById('news').style.display = 'none';
   }
   news++;
