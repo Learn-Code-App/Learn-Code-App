4d1e38e13d88994f3ee5eaceb4fca4221b89e184
code-alt <54744445+code-alt@users.noreply.github.com>
2020-10-24 15:38:11 -0500

    Delete help.js

--- help/help.js
+++
@@ -1,7 +0,0 @@
-var icon = document.getElementById("nav-icon4");
-document.getElementById("nav-icon4").onclick = function(){
-	document.getElementById("nav-icon4").classList.add("open");
-  window.setTimeout(function () {
-  document.getElementById("nav").style.display = "block";
-  },500);
-};
